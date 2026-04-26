1cbd13de29f768520873fdc5e4169c8df7f32553
valeryarg <baleriarg98@gmail.com>
2026-04-26 14:51:21 -0600

    index

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
 
 app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log('Servidor corriendo en puerto ' + PORT);
+  console.log('Servidor corriendo en ' + PORT);
 });
