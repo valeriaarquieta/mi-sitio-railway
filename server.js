d0e310f3a943a6c69f16d44d5b7ff79f5c7aa49a
valeryarg <baleriarg98@gmail.com>
2026-04-26 14:43:30 -0600

    Agrego server y configuración para deploy

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Servidor corriendo en puerto ' + PORT);
+});
